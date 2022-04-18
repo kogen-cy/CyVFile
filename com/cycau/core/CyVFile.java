017fd2ba30f09f0007077ef6920b861009684599
kogen-cy <76019908+kogen-cy@users.noreply.github.com>
2022-04-18 17:13:25 +0900

    Update CyVFile.java

--- com/cycau/core/CyVFile.java
+++ com/cycau/core/CyVFile.java
@@ -22,12 +22,12 @@ import java.util.List;
 public class CyVFile {
 //	private final char SPLIT = '`';
 	private final short MAX_COLUMN_LENGHT = 50;
-	private final byte[] LEN2CHAR = new byte[]{
+	private final byte[] LEN2CHAR = {
 		 '0','1','2','3','4','5','6','7','8','9'
 		,'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'
 		,'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'
 	};
-	private final short[] CHAR2LEN = new short[]{
+	private final short[] CHAR2LEN = {
 		-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1
 		,0,1,2,3,4,5,6,7,8,9
 		,-1,-1,-1,-1,-1,-1,-1
